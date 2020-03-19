6215a3cfc75e613b2001fed968c02b6859a15958
Alberto <AlberHanibal@sobremesa.com>
2020-03-19 18:01:54 +0100

    Funcionalidad descripcion, jugar

---
+++ view/data/infoJuegos.js
@@ -0,0 +1,15 @@
+/* titulo tiene que ser igual a las carpetas, usandolo como href */
+var infoJuegos = {
+    "PuzzleBobble": {
+        "Titulo": "PuzzleBobble",
+        "Descripcion": "Un videojueguito muy bonito",
+        "Controles": "",
+        "Explicacion": ""
+    },
+    "Arkanoid": {
+        "Titulo": "Arkanoid",
+        "Descripcion": "",
+        "Controles": "",
+        "Explicacion": ""
+    }
+}
