f4ea6ce01e17315e66e528ff3893d3a40a608995
charly30000 <andresbuzon1@hotmail.com>
2020-03-19 16:59:48 +0100

    haciendo las cosas en plan monas

--- view/js/indice.js
+++
@@ -1,6 +0,0 @@
-let botonJugarPuzzleBobble = document.getElementById("jugarPuzzleBobble");
-console.log(botonJugarPuzzleBobble);
-botonJugarPuzzleBobble.addEventListener("click", function () {
-    console.log("click");
-    // cargarJuego("../../Games/PuzzleBobble/index.html");
-});
