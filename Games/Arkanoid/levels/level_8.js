99b1e4dc02b799b15cb72700c99233ea78a22036
charly30000 <andresbuzon1@hotmail.com>
2020-04-06 20:31:35 +0200

    Template de los niveles creado

---
+++ Games/Arkanoid/levels/level_8.js
@@ -0,0 +1,10 @@
+var level_8 = 
+{
+    enemy_type: "",
+    background: "",
+    music: "",
+    ladrillos: [
+        [{color: "", movement: false}, {color: "", movement: false},], // Primera fila
+        [{color: "", movement: false}, {color: "", movement: false},], // Segunda fila
+    ]
+}
