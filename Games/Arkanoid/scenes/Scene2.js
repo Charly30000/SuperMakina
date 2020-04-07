8fb39ae9c02cec4c8ff9ed17d52b5abf2dfa1451
charly30000 <andresbuzon1@hotmail.com>
2020-04-07 21:32:23 +0200

    Cambios menores del fin del dia 07/04/2020

--- Games/Arkanoid/scenes/Scene2.js
+++ Games/Arkanoid/scenes/Scene2.js
@@ -42,55 +42,57 @@ class Scene2 extends Phaser.Scene {
                 this.scene.restart();
                 break;
         }
+        if (nivel >= 1 || nivel <= 10) {
+            // Colocacion del fondo
+            /************** 
+                POR HACER
+            ***************/
 
-        // Colocacion del fondo
-        /************** 
-            POR HACER
-        ***************/
+            // Colocacion de barras
+            this.listaBarras = this.physics.add.staticGroup();
+            this.listaBarras.add(this.add.sprite(30, 48, "barra_arriba").setOrigin(0, 0));
+            this.listaBarras.add(this.add.sprite(0, 68, "barra_lateralizda").setOrigin(0, 0));
+            this.listaBarras.add(this.add.sprite(376, 68, "barra_lateraldcha").setOrigin(0, 0));
+            this.add.sprite(366, 49, "barra_girodcha").setOrigin(0, 0);
+            this.add.sprite(0, 49, "barra_giroizda").setOrigin(0, 0);
 
-        // Colocacion de barras
-        this.listaBarras = this.physics.add.staticGroup();
-        this.listaBarras.add(this.add.sprite(30, 48, "barra_arriba").setOrigin(0, 0));
-        this.listaBarras.add(this.add.sprite(0, 68, "barra_lateralizda").setOrigin(0, 0));
-        this.listaBarras.add(this.add.sprite(376, 68, "barra_lateraldcha").setOrigin(0, 0));
-        this.listaBarras.add(this.add.sprite(366, 49, "barra_girodcha").setOrigin(0, 0));
-        this.listaBarras.add(this.add.sprite(0, 49, "barra_giroizda").setOrigin(0, 0));
+            // Parte de las puntuaciones
+            var graphics = this.add.graphics();
+            graphics.fillStyle("Black");
+            graphics.fillRect(0, 0, config.width, 48);
 
-        // Parte de las puntuaciones
-        var graphics = this.add.graphics();
-        graphics.fillStyle("Black");
-        graphics.fillRect(0, 0, config.width, 48);
-
-        // Sistema de colocacion de ladrillos
-        var posX = 40;
-        var posY = 80;
-        this.listaLadrillos = this.physics.add.group();
-        cargaNivel.ladrillos.forEach(arrayLadrillos => {
-            arrayLadrillos.forEach(element => {
-                if (element.ladrillo){
-                    this.listaLadrillos.add(new Ladrillo(this, posX, posY, element.ladrillo, element.movement));
-                }
-                posX += 32;
+            // Sistema de colocacion de ladrillos
+            var posX = 40;
+            var posY = 80;
+            this.listaLadrillos = this.physics.add.group();
+            cargaNivel.ladrillos.forEach(arrayLadrillos => {
+                arrayLadrillos.forEach(element => {
+                    if (element.ladrillo) {
+                        this.listaLadrillos.add(new Ladrillo(this, posX, posY, element.ladrillo, element.movement));
+                    }
+                    posX += 32;
+                });
+                posX = 40;
+                posY += 16;
             });
-            posX = 40;
-            posY += 16;
-        });
 
-        // Pelotas
-        this.listaPelotas = this.physics.add.group();
-        this.listaPelotas.add(new Pelota(this, config.width / 2, config.height / 2, "bola_roja"));
-        this.listaPelotas.setVelocity(Phaser.Math.Between(-120, 120), Phaser.Math.Between(-120, 120));
-        
-        // Jugador
+            // Pelotas
+            this.listaPelotas = this.physics.add.group();
+            this.listaPelotas.add(new Pelota(this, config.width / 2, config.height / 2, "bola_roja"));
+            this.listaPelotas.setVelocity(Phaser.Math.Between(-120, 120), Phaser.Math.Between(-120, 120));
 
-        /**************** 
-            COLISIONES
-        *****************/
-        // Colisiones pelota
-        this.physics.add.collider(this.listaPelotas, this.listaLadrillos, function(pelota, ladrillo) {
-            ladrillo.destroy();
-        });
-        this.physics.add.collider(this.listaPelotas, this.listaBarras);
+            // Jugador
+
+            /**************** 
+                COLISIONES
+            *****************/
+            // Colisiones pelota
+            this.physics.add.collider(this.listaPelotas, this.listaLadrillos, function (pelota, ladrillo) {
+                ladrillo.destroy();
+            });
+            this.physics.add.collider(this.listaPelotas, this.listaBarras);
+        }
+
     }
 
     update() {
