cf4536233fb796edce466a0f2b22f42d749b170f
Alberto <AlberHanibal@sobremesa.com>
2020-04-08 22:07:15 +0200

    Background y bordes a png, eliminados los bmp

--- Games/PuzzleBobble/src/js/bootloader.js
+++ Games/PuzzleBobble/src/js/bootloader.js
@@ -18,14 +18,14 @@ class Bootloader extends Phaser.Scene {
         this.load.image("burbujaRo", "./assets/images/bolas/bolaroja.png");
         this.load.image("burbujaVe", "./assets/images/bolas/bolaverde.png");
         this.load.image("inicio", "./assets/images/pantallainicial/titulo.jpg");
-        this.load.image("fondo1-3", "./assets/images/Backgrounds/Background 1-3.bmp");
-        this.load.image("fondo4-6", "./assets/images/Backgrounds/Background 4-6.bmp");
-        this.load.image("fondo7-9", "./assets/images/Backgrounds/Background 7-9.bmp");
-        this.load.image("fondo10-12", "./assets/images/Backgrounds/Background 10-12.bmp");
+        this.load.image("fondo1-3", "./assets/images/Backgrounds/Background 1-3.png");
+        this.load.image("fondo4-6", "./assets/images/Backgrounds/Background 4-6.png");
+        this.load.image("fondo7-9", "./assets/images/Backgrounds/Background 7-9.png");
+        this.load.image("fondo10-12", "./assets/images/Backgrounds/Background 10-12.png");
         this.load.image("borde1-3", "./assets/images/Backgrounds/Borders 1-3.png");
         this.load.image("borde4-6", "./assets/images/Backgrounds/Borders 4-6.png");
-        this.load.image("borde7-9", "./assets/images/Backgrounds/Borders 7-9.bmp");// problemas con este
-        this.load.image("borde10-12", "./assets/images/Backgrounds/Borders 10-12.bmp"); // problemas con este
+        this.load.image("borde7-9", "./assets/images/Backgrounds/Borders 7-9.png");
+        this.load.image("borde10-12", "./assets/images/Backgrounds/Borders 10-12.png");
         // crea la barra de porcentaje
         var loadingBar = this.add.graphics({
             fillStyle: {
