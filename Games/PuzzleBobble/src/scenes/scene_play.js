4e255d5b0b043644c28994148ae45e1502d205cc
sandra1105 <sandrasrs00@gmail.com>
2020-04-09 23:38:36 +0200

    colision con los lados funciona

--- Games/PuzzleBobble/src/scenes/scene_play.js
+++ Games/PuzzleBobble/src/scenes/scene_play.js
@@ -1,8 +1,9 @@
+var gradosx;
+var arrayburbujas;
 class Scene_play extends Phaser.Scene {
     constructor() {
         super({ key: "Scene_play" })
     }
-
     create() {
         this.flecha = this.add.sprite(this.sys.game.config.width / 2, 500, 'flechas');
         this.anims.create({
@@ -11,20 +12,21 @@ class Scene_play extends Phaser.Scene {
             frameRate: 10,
             repeat: -1
         });
-        
+
         this.anims.create({
             key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
+            frames: [{ key: 'dude', frame: 4 }],
             frameRate: 20
         });
-        
+
         this.anims.create({
             key: 'right',
             frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
             frameRate: 10,
-            repeat: -1 
+            repeat: -1
         });
         var nivel;
+        var valorx;
         //comprueba en la variable global del init en que numero de nivel nos encontramos y coge del nivel(numero).js toda la informacion
         switch (numeronivel) {
             case 1:
@@ -65,7 +67,7 @@ class Scene_play extends Phaser.Scene {
         this.add.image(450, 300, nivel.fondo).setScale(3);
         this.add.image(this.sys.game.config.width / 2, 300, nivel.borde).setScale(3);
         // crea el array de todas las filas del nivel para guardar las referencias de las burbujas
-        var arrayburbujas = new Array(this.crearfila(8), this.crearfila(7), this.crearfila(8), this.crearfila(7), this.crearfila(8), this.crearfila(7),
+        arrayburbujas = new Array(this.crearfila(8), this.crearfila(7), this.crearfila(8), this.crearfila(7), this.crearfila(8), this.crearfila(7),
             this.crearfila(8), this.crearfila(7), this.crearfila(8), this.crearfila(7), this.crearfila(8));
 
         let x = 0;
@@ -122,11 +124,10 @@ class Scene_play extends Phaser.Scene {
             }
         }
         // se crean las dos bolas de lanzar
-        this.lanzarbola = this.crearbolalanzar(arrayburbujas,this.sys.game.config.width / 2, 525);
-        console.log(this.lanzarbola);
-        this.lanzarbolasegunda = this.crearbolalanzar(arrayburbujas,this.sys.game.config.width / 2.4,600);
-        console.log(arrayburbujas);
+        this.lanzarbola = this.crearbolalanzar(this.sys.game.config.width / 2, 525);
+        this.lanzarbolasegunda = this.crearbolalanzar(this.sys.game.config.width / 2.4, 600);
         this.cursor_space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
     }
 
     // crea un array con 7 o 8 sitios
@@ -140,7 +141,7 @@ class Scene_play extends Phaser.Scene {
 
 
     // mira todo el array de burbujas y devuelve un array con los colores que tiene el array
-    colorbolanueva(arrayburbujas) {
+    colorbolanueva() {
         let arrayburbujaslanzar = new Array();
         for (let i = 0; i < arrayburbujas.length; i++) {
             for (let j = 0; j < arrayburbujas[i].length; j++) {
@@ -159,44 +160,44 @@ class Scene_play extends Phaser.Scene {
 
 
     // crea una nueva bola con el color que le pasa colorbolanueva
-    crearbolalanzar(arrayburbujas,x,y) {
+    crearbolalanzar(x, y) {
         let colorlanzar = this.colorbolanueva(arrayburbujas);
-        return new BurbujaMovil(this, x , y, "burbuja" + colorlanzar, colorlanzar).setScale(3);
-        this.annadircolisionburbujas(arrayburbujas);
+        this.annadircolisionburbujas();
+        return new BurbujaMovil(this, x, y, "burbuja" + colorlanzar, colorlanzar).setScale(3);
     }
 
 
-    annadircolisionburbujas(arrayburbujas) {
+    annadircolisionburbujas() {
         for (let i = 0; i < arrayburbujas.length; i++) {
             for (let j = 0; j < arrayburbujas[i].length; j++) {
                 if (arrayburbujas[i][j] != " ") {
-                    this.physics.add.collider(this.lanzarbola, arrayburbujas[i][j], this.colisionPelotas, null, this);
+                    //no funciona
+                    //this.physics.add.collider(this.lanzarbola, arrayburbujas[i][j], this.colisionPelotas,null,this);
 
                 }
             }
         }
     }
-    colisionPelotaBorde() {
-        console.log("ayuda");
-        //this.lanzarbola.body.velocity.set(-110,-120);
-    }
+
     colisionPelotas() {
         console.log("siiii");
     }
     update() {
-        var valorx;
+
         if (this.cursor_space.isDown) {
-            this.lanzarbola.body.velocity.set(-110,-10);
-            console.log(this.lanzarbola.velocity);
-            valorx = -this.lanzarbola.body.velocity.x;
+            // para darle velocidad si la pelota aun no ha sido lanzada
+            if (this.lanzarbola.body.velocity.x == 0 && this.lanzarbola.body.velocity.y == 0) {
+                this.lanzarbola.body.velocity.set(-110, -10);
+                gradosx = -this.lanzarbola.body.velocity.x;
+            }
         }
-        if(this.lanzarbola.x < this.sys.game.config.width / 3.2) {
-            console.log(this.lanzarbola.body.velocity.get());
-            this.lanzarbola.body.velocity.set(100, this.lanzarbola.body.velocity.y);
-            
-        }else if(this.lanzarbola.x > this.sys.game.config.width / 3.2 + 335) {
-            this.lanzarbola.body.velocity.set(valorx ,this.lanzarbola.body.velocity.y);
-        } 
+        // para cambiar la velocidad si choca con el lado izquierdo
+        if (this.lanzarbola.x < this.sys.game.config.width / 3.2) {
+            this.lanzarbola.body.velocity.set(gradosx, this.lanzarbola.body.velocity.y);
+        // para cambiar la velocidad si choca con el lado derecho
+        } else if (this.lanzarbola.x > this.sys.game.config.width / 3.2 + 335) {
+            this.lanzarbola.body.velocity.set(-gradosx, this.lanzarbola.body.velocity.y);
+        }
         //this.scene.restart();
     }
 }
