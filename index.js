eb0f4339d2f60e69150161920de6e789a05facbc
Alberto <AlberHanibal@sobremesa.com>
2020-04-12 19:28:36 +0200

    Interfaz dark, logo

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.on('ready', function () {
             nodeIntegration: true
         },
         width: 1600,
-        height: 900
+        height: 900,
+        darkTheme: true
     })
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'view/index.html'),
@@ -34,6 +35,7 @@ app.on('ready', function () {
         app.quit();
     })
     mainWindow.maximize();
+    // mainWindow.removeMenu(); // para quitar el menu cuando se vaya a entregar
 })
 
 // *****no se puede quitar, el menu debe tener al menos una pestaña, rol o tipo
@@ -56,12 +58,14 @@ function crearVentanaPuntuaciones() {
             width: 800,
             height: 400,
             title: 'Puntuaciones',
+            darkTheme: true,
+            maximizable: false,
+            resizable: false,
             webPreferences: {
                 nodeIntegration: true
             },
             show: false
         });
-        //ventanaPuntuaciones.setMenu(null);
         ventanaPuntuaciones.loadURL(url.format({
             pathname: path.join(__dirname, 'view/ventanaPuntuaciones.html'),
             protocol: 'file',
@@ -72,8 +76,7 @@ function crearVentanaPuntuaciones() {
             mainWindow.setEnabled(true);
             mainWindow.show();
         })
-        ventanaPuntuaciones.setMaximizable(false);
-        ventanaPuntuaciones.setResizable(false);
+        // ventanaPuntuaciones.removeMenu(); // para quitar el menu cuando se vaya a entregar
     }
 }
 
