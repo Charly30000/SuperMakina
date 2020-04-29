f4331550686df99a49adc68804d817638a0fff1e
Alberto <AlberHanibal@sobremesa.com>
2020-04-29 19:05:11 +0200

    Interfaz del juego

---
+++ Games/Arkanoid/indice.js
@@ -0,0 +1,2 @@
+document.getElementById("controles").textContent = "Controles: " + infoJuegos.Arkanoid.Controles;
+document.getElementById("explicacion").textContent = infoJuegos.Arkanoid.Explicacion;
