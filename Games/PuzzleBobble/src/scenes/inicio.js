c0d8f708a3e842bd6345dd20b4eafb395d362e76
Alberto <AlberHanibal@sobremesa.com>
2020-05-08 17:01:50 +0200

    Imagen inicial PuzzleBobble

--- Games/PuzzleBobble/src/scenes/inicio.js
+++ Games/PuzzleBobble/src/scenes/inicio.js
@@ -6,7 +6,7 @@ class Inicio extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(450, 250, 'inicio').setScale(3);
+        this.add.image(450, 250, 'inicio');
         this.scoreLabel = this.add.text(300, 550, `Presiona Espacio para comenzar`);
         this.cursor_empezar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
