015aa312bf6a80ddf576bf22325845462d593359
Alberto <AlberHanibal@sobremesa.com>
2020-05-24 13:17:20 +0200

    Tildes en controles

--- view/data/infoJuegos.js
+++ view/data/infoJuegos.js
@@ -1,4 +1,4 @@
-/* titulo tiene que ser igual a las carpetas, usandolo como href */
+/* titulo tiene que ser igual a las carpetas, usándolo como href */
 /*
     Informacion sobre los juegos
 */
@@ -6,13 +6,13 @@ var infoJuegos = {
     "PuzzleBobble": {
         "Titulo": "PuzzleBobble",
         "Descripcion": "PUZZLE BOBBLE es un juego de acción y rompecabezas. ¡Bub y Bob, los dragones que escupen burbujas del juego de acción BUBBLE BOBBLE, están de vuelta en un nuevo rompecabezas basado en burbujas!",
-        "Controles": "Flechas de direccion izquierda y derecha, espacio para disparar",
+        "Controles": "Flechas de dirección izquierda y derecha, espacio para disparar",
         "Explicacion": "Haz rebotar las burbujas de las paredes con destreza y combina 3 o más burbujas del mismo color para eliminarlas. Elimina todas las burbujas para pasar de nivel, si las burbujas llegan a la altura de la linea pierdes."
     },
     "Arkanoid": {
         "Titulo": "Arkanoid",
         "Descripcion": "¡Destruye bloques con una bola de demolición! Tu trabajo consistirá en conseguir el mayor número de puntos rompiendo los distintos ladrillos, ¡Pero cuidado! ¡Caen sorpresas cuando los rompes!",
-        "Controles": "Flechas de direccion izquierda y derecha, espacio para comenzar",
+        "Controles": "Flechas de dirección izquierda y derecha, espacio para todo lo demás",
         "Explicacion": "El jugador controla una pequeña plataforma, que impide que una bola salga de la zona de juego, haciéndola rebotar. En la parte superior hay \"ladrillos\" o \"bloques\", que desaparecen al ser tocados por la bola. Ganas cuando rompes todos los ladrillos."
     }
 }
